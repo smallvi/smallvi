05d4945765cc56c1d5dd78a99b9fb84b8b10a842
smallvi <smallvi@gmail.com>
2024-08-01 00:17:36 +0800

    feat: add scroll animation

--- scripts.js
+++ scripts.js
@@ -1,21 +1,36 @@
+// Click to scroll
 function scrollToAnchor(aid) {
-    var aTag = $("#" + aid);
+    var aTag = $(aid);
     $('html,body').animate({ scrollTop: aTag.offset().top }, 'slow');
 }
 
 $('.nav-link').on('click', function (e) {
     e.preventDefault();
 
-    // Navigation
-    $('.nav-link').removeClass('font-black');
-    $('.nav-link').removeClass('text-gray-100');
-    $('.nav-link').removeClass('scale-125');
+    $('.nav-link').removeClass('font-black text-gray-100 scale-125');
 
-    $(this).addClass('font-black');
-    $(this).addClass('text-gray-100');
-    $(this).addClass('scale-110');
+    $(this).addClass('font-black text-gray-100 scale-125');
 
     const targetId = $(this).attr('href');
-    // alert(targetId);
     scrollToAnchor(targetId);
+});
+
+// Scroll effect navigation
+var sections = $('div[id^="section"]');
+var navLinks = $('.nav-link');
+
+$(window).on('scroll', function () {
+    var currentScroll = $(this).scrollTop();
+
+    sections.each(function () {
+        var sectionTop = $(this).offset().top - 70; // Adjust for fixed nav height
+        var sectionBottom = sectionTop + $(this).outerHeight();
+
+        if (currentScroll >= sectionTop && currentScroll < sectionBottom) {
+            var id = $(this).attr('id');
+
+            navLinks.removeClass('font-black text-gray-100 scale-125');
+            $('a[href="#' + id + '"]').addClass('font-black text-gray-100 scale-125');
+        }
+    });
 });

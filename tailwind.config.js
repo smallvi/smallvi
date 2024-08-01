d0c1c65df4fad607d9714296049b5309ed37b9f6
Yeo <smallvi@gmail.com>
2024-08-01 11:38:08 +0800

    feat: install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./scripts.js",
+    "./styles.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
